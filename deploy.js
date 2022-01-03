34708e0ac22f9358e5e13ebd340f175c2f3f8854
Oscar Flores <90513703+ojoflow@users.noreply.github.com>
2022-01-03 02:32:56 +0000

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'joy bubble pause urban honey alter seven dove shallow sun change impact', //seed phrase
+  'https://rinkeby.infura.io/v3/ff4569ac892642daba891e64583227e1'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: evm.bytecode.object})
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
